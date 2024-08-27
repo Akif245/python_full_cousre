c87da2a1fe8cebcc006a121b377495ceb40c18aa
Akif245 <abdulakif44570@gmail.com>
2024-08-27 22:50:56 +0530

    Complete Hungman Game

--- hangman.py
+++ hangman.py
@@ -1,24 +1,57 @@
 import random
-word_list=["akif","ahmed","abdul"]
 
-chosen_word=random.choice(word_list)
-print(chosen_word)
-guess=input("Guess a letter\n").lower()
-#print(guess)
+from hangman_words import word_list
 
-for letter in chosen_word:
-    if letter==guess:
-        print("Right")
-    else:
-        print("Wrong")
-display=[]
+chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+
+from hangman_art import logo
+print(logo)
+
+
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
 for _ in range(word_length):
     display += "_"
-print(display)
-guess=input("Guess a letter\n").lower()
-for position in range(word_length):
-    letter = chosen_word[position]
-if letter==guess:
-    display[position]=letter
-print(display)
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    
+    for position in range(word_length):
+        letter = chosen_word[position]
+    
+        if letter == guess:
+            display[position] = letter
+
+    
+    if guess not in chosen_word:
+       
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    
+    print(f"{' '.join(display)}")
+
+    
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    
+    from hangman_art import stages
+    print(stages[lives])
