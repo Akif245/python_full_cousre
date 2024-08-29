abb9483e42c58648ffdcc787510a77279e896795
Akif245 <abdulakif44570@gmail.com>
2024-08-29 23:16:44 +0530

    Caesar cipher

---
+++ Cipher.py
@@ -0,0 +1,58 @@
+alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s'
+          ,'t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s'
+          ,'t','u','v','w','x','y','z']
+direction=input("Type '1' to to encrypt ,Type '2' to decrypt\n")
+
+text=input("Type your message \n").lower()
+
+shift=int(input("Type the number to shift \n"))
+if direction=="1".lower():
+    def encrypt(usertext,Shiftnum):
+        cipher_letter=""
+        for letter in usertext:
+            position=alphabet.index(letter)
+            new_pos=position+Shiftnum
+            new_letter=alphabet[new_pos]
+            cipher_letter=cipher_letter+new_letter
+        print(f"The Encrypted letter is {cipher_letter}")
+
+    encrypt(text,shift)
+
+elif direction=="2".lower():
+    def decrypt(usertext1,Shiftnum1):
+        cipher_letter1=""
+        for letter in usertext1:
+            position=alphabet.index(letter)
+            new_pos=position-Shiftnum1
+            new_letter=alphabet[new_pos]
+            cipher_letter1=cipher_letter1+new_letter
+        print(f"The Decrypted letter is {cipher_letter1}")
+
+    decrypt(usertext1=text,Shiftnum1=shift)
+else:
+   print("You have Intered wrong direction\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
