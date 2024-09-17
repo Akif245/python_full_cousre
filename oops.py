264c21060c7b6636361331f96d2211f455c134ad
Akif245 <abdulakif44570@gmail.com>
2024-09-18 00:11:55 +0530

    prettTable module

---
+++ oops.py
@@ -0,0 +1,6 @@
+from prettytable import *
+from prettytable import PrettyTable
+table=PrettyTable()
+table.add_column("Pokemon",["pikachu","bulbasor","charizard"])
+table.add_column("Type",["Electric","grass","fire"])
+print(table)
