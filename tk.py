a9fb06000ab60e80085195d2e3a935569a3cdec3
Akif245 <abdulakif44570@gmail.com>
2025-02-25 23:44:03 +0530

    Tkinter

--- tk.py
+++ tk.py
@@ -1,5 +1,9 @@
-import tkinter
-window=tkinter.Tk()
+import tkinter as tk
+window=tk.Tk()
+window.title("Tkinter window")
+window.minsize(width=500,height=500)
+l1=tk.Label(text="This is a lable",font=("Arial ",20 ,"bold"))
+l1.pack()
 
 
 
