422423eeeb17e9020fa2c0621d59244600379064
Akif245 <abdulakif44570@gmail.com>
2025-04-12 00:47:49 +0530

    Tkinter

--- tk.py
+++ tk.py
@@ -1,9 +1,25 @@
-import tkinter as tk
-window=tk.Tk()
+from tkinter import *
+window=Tk()
 window.title("Tkinter window")
 window.minsize(width=500,height=500)
-l1=tk.Label(text="This is a lable",font=("Arial ",20 ,"bold"))
+
+
+l1=Label(text="This is a lable",font=("Arial ",20 ,"bold"))
 l1.pack()
+
+l1["text"]="New text"
+l1.config(text="New text ")
+
+def button():
+    print("I got clicked")
+    l1.config(text=input.get())
+btn=Button(text="Click me",command=button)
+btn.pack()
+input=Entry(width="20")
+input.pack()
+
+
+
 
 
 
@@ -14,3 +30,5 @@ l1.pack()
 
 
 window.mainloop()
+
+
